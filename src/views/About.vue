6f14b90db4b30ddc333785cb60b19a3c4a1d8f51
Jessica Lou <jessicalou@Jessicas-MacBook-Pro.local>
2020-05-01 08:36:37 -0700

    jess merge

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,62 +4,35 @@
     <h1 style="text-align: center;">husky hub</h1>
     <p style="text-align: center; width: 45%; margin-left: auto; margin-right: auto;">Improving the student food service experience regardless of financial standing. Building community through on campus resources and small business owners.</p>
     <br><br><br>
-<<<<<<< HEAD
-    <h2 class="encode-sans">THE PROBLEM</h2>
-    <p>Students are given little options in terms of how they can spend their dining account funds. Current food selections are either overpriced or unsatisfactory.</p>
-    <br>
-    <h2 class="encode-sans">OUR SOLUTION</h2>
-=======
     <h2>THE PROBLEM</h2>
     <p>Students are given little options in terms of how they can spend their dining account funds. Current food selections are either overpriced or unsatisfactory.</p>
     <br>
     <h2>OUR SOLUTION</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
     <p>This new dining plan will give more autonomy over what they eat and how they spend their dining account funds.</p>
     <p>Our team proposes a platform that broadens the UW Dining experience to local businesses, thus providing students with more choices for where they can spend their money.</p>
     <p>This platform will also provide students with the chance to make informed spending choices by providing lists of participating vendors and information on their own spending habits.</p>
     <br>
-<<<<<<< HEAD
-    <h2 class="encode-sans">OUR TEAM</h2>
-=======
     <h2>OUR TEAM</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
     <b-container id="team">
       <b-row>
         <b-col class="member">
           <img src="../assets/team/alex.png">
-<<<<<<< HEAD
-          <h2 class="encode-sans">Alex Park</h2>
-=======
           <h2>Alex Park</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
           <p>project manager</p>
         </b-col>
         <b-col class="member">
           <img src="../assets/team/jess.png">
-<<<<<<< HEAD
-          <h2 class="encode-sans">Jessica Lou</h2>
-=======
           <h2>Jessica Lou</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
           <p>designer</p>
         </b-col>
         <b-col class="member">
           <img src="../assets/team/andrea.png">
-<<<<<<< HEAD
-          <h2 class="encode-sans">Andrea Koozer</h2>
-=======
           <h2>Andrea Koozer</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
           <p>data scientist</p>
         </b-col>
         <b-col class="member">
           <img src="../assets/team/kev.png">
-<<<<<<< HEAD
-          <h2 class="encode-sans">Kevin Zhang</h2>
-=======
           <h2>Kevin Zhang</h2>
->>>>>>> c1e1a518f375715247d7815589d36de6ca0cd64c
           <p>developer</p>
         </b-col>
       </b-row>
